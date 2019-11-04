3f891c336de13be2514991d288d4357d9fdf3d08
Skymorii <hergerthlea@gmail.com>
2019-11-04 11:55:48 +0100

    Favicon and buttons added, more bugs to fix added

--- client/src/components/classrooms/ClassroomList.js
+++ client/src/components/classrooms/ClassroomList.js
@@ -11,25 +11,44 @@ export default class ClassroomList extends Component {
             classrooms: []
         }
     }
-
+    
     async componentDidMount() {
         let classrooms = [];
-        await Axios.get("http://localhost:4000/classrooms")
+        await Axios.get(`http://localhost:4000/classrooms`)
              .then(response => {
                  response.data.forEach(classroom => {
                     classrooms.push(<ClassroomComponent classroom = {classroom}/>);
                  });
              })
-             .catch(error => { console.log("ClassroomList componentDidMount error") });
+             .catch(error => { console.log("Error in ClassroomList componentDidMount") });
+        this.setState({ classrooms: classrooms });
+    };
+    
+    async changeOrderClassroom(orderby, e) {
+        e.preventDefault();
+        let classrooms = [];
+        await Axios.get(`http://localhost:4000/classrooms/${orderby}`)
+        .then(response => {
+            response.data.forEach(classroom => {
+                classrooms.push(<ClassroomComponent classroom = {classroom}/>);
+            });
+        })
+        .catch(error => { console.log("Error in ClassroomList changeOrderClassroom") });
         this.setState({ classrooms: classrooms });
     }
-
+    
     render() {
         return (
             <main id="classroomspage">
                 <div>
                     <h1>Tantermek</h1>
-                    <h2>Összes tanterem listája</h2>
+                    <div id="orderbuttons">
+                        <h2>Lista rendezése</h2>
+                        <button onClick={(e) => this.changeOrderClassroom("teremszam", e)}>Teremszám</button>
+                        <button onClick={(e) => this.changeOrderClassroom("kapacitas", e)}>Kapacitás</button>
+                        <button onClick={(e) => this.changeOrderClassroom("gepterem_e", e)}>Gépterem</button>
+                    </div>
+                    <h2>Tantermek listája</h2>
                     <table>
                         <tbody>
                             <tr>
